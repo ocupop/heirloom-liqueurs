b90d79889003fab8d73c2f1b7932a7b2d8ab1cba
Jon Burr <yojonathanwburr@gmail.com>
2018-10-23 13:35:11 -0700

    updates to product images

--- src/assets/_js/App/components/RecipeDetail.js
+++ src/assets/_js/App/components/RecipeDetail.js
@@ -25,7 +25,6 @@ class RecipeDetail extends Component {
       RECIPES = RECIPES.filter((recipe) => recipe.slug == this.state.param );
       console.log(RECIPES[0].liqueurs);
       RELATED = RELATED.filter((recipe) => recipe.liqueurs.includes(RECIPES[0].liqueurs) && recipe != RECIPES[0]);
-      // RELATED = RELATED.filter((recipe) => recipe != RECIPES[0]);
       this.setState({ 
         recipe: RECIPES[0],
         relatedRecipes: RELATED
